4dd0ed259600393af724b0065624cd131ab39aab
赵子豪 <zihao13969918264@hotmail.com>
2019-09-22 21:23:26 +0800

    Update largest_prime_factor.c

--- 3/largest_prime_factor.c
+++ 3/largest_prime_factor.c
@@ -5,20 +5,29 @@
 /*	赵子豪@2019年9月22日	*/
 /*	largest_prime_factor.c	*/
 #include <stdio.h>
+#include <math.h>
 
 #define TARGET 600851475143
 
-int max_prime_factors(int);
+int max_prime_factor(int);
 
 int main(void)
 {
-	printf("The largest prime factor of %d is %d.\n", TARGET, max_prime_factors(TARGET));
+	printf("The largest prime factor of %d is %d.\n", TARGET, max_prime_factor(TARGET));
 
 	return 0;
 }
 
-int max_prime_factors(int target)
+int max_prime_factor(int target)
 {
 	int n = 1;
+	int largest = 0;
+	
+	while( n <= sqrt(target))
+	{
+		if ((n % 10) == 0)
+			printf("\n");
 
+		printf("%d.\t")
+	}
 }
