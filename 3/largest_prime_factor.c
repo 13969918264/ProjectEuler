06b46a28bae283b6a7f7b625fe04a9a99b8668a5
赵子豪 <zihao13969918264@hotmail.com>
2019-09-24 22:14:34 +0800

    Update largest_prime_factor.c

--- 3/largest_prime_factor.c
+++ 3/largest_prime_factor.c
@@ -2,7 +2,7 @@
 	The prime factors of 13195 are 5, 7, 13 and 29.
 	What is the largest prime factor of the number 600851475143 ?
 */
-/*	赵子豪@2019年9月22日23日	*/
+/*	赵子豪@2019年9月22日23日24日	*/
 /*	largest_prime_factor.c	*/
 #include <stdio.h>
 #include <math.h>
@@ -13,6 +13,7 @@
 
 void max_prime_factor(int *);
 int * factors(int);
+int * sort(int *);
 
 int main(void)
 {
@@ -21,22 +22,39 @@ int main(void)
 	return 0;
 }
 
-int max_prime_factor(int target)
+int * factors(int target)
 {
 	int n = 1;
 	int count = 0;
 	int factors[MAX];
+	int * p;
 
+	factors[MAX - 1] = 0;
 	while( n <= sqrt(target))
 	{
 		if ((target % n) == 0)
 		{
+			factors[count++] = n;
 			factors[count++] = target / n;
 			n++;
 			continue;
 		}
-
-		if (floor(sqrt(target)) == ceil(sqrt(target)))
-			factors[MAX] = sqrt(target);
+		if ((factors[MAX - 1] == 0) && (floor(sqrt(target)) == ceil(sqrt(target)))
+			factors[MAX - 1] = sqrt(target);
+		if (factors[MAX - 1] != 0)
+			factors[count++] = factors[MAX - 1];
 	}
+	p = sort(factors);
+
+	return p;
+}
+
+int * sort(int * p)
+{
+
+}
+
+void max_prime_factor(int * p)
+{
+
 }
